8e508ae059610286f102243695eca3e1d99dfd96
crazyitalian1679 <ajscimone@live.com>
2017-08-29 20:47:40 +0000

    V2, data peristence added

--- app.js
+++ app.js
@@ -1,49 +1,95 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-    ]
+//Schema setup
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//      {
+//          name: "Granite Hill", 
+//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//          description: "This is a huge granite hill, no bathrooms, no water, beautiful granite!"
+         
+//      },
+//      function(err, campground){
+//       if(err){
+//           console.log(err);
+//       } else {
+//           console.log("NEWLY CREATED CAMPGROUND: ");
+//           console.log(campground);
+//       }
+//     });
 
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if (err){
+            console.log(err);
+        }
+        else{
+                res.render("index", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
+//CREATE - add new campground to database
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+    //Create and save a new campground
+    Campground.create(newCampground, function(err, newlyCreated){
+        if (err){
+            console.log("err");
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
    res.render("new.ejs"); 
 });
 
 
+//SHOW - shows info on a campground
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err)
+        }
+        else{
+               res.render("show", {campground: foundCampground}); 
+        }
+    })
+});
 
-// app.get("*", function(req, res){
-//     res.send("Sorry, page not found");
-// });
+
+//Page not found route
+ app.get("*", function(req, res){
+     res.send("Sorry, page not found");
+ });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Listening");
