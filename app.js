91f9d67fecb052b37bcdc3dfc85b79a634e7f6ed
crazyitalian1679 <ajscimone@live.com>
2017-09-13 05:28:07 +0000

    Switch to mLab

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes    = require("./routes/comments.js"),
 
 //set up database
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://AJScimone:Chill1679!@@!@ds135514.mlab.com:35514/gocamp", {useMongoClient: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
